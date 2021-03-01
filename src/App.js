385ebe4f7535d0371927aa693fa49e040fa71bf9
Lukasz Sadurski <lukaszsadurski@gmail.com>
2021-03-01 21:52:01 +0100

    Project setup

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import { Button } from 'antd';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <Button>Default Button</Button>
         <a
           className="App-link"
           href="https://reactjs.org"
