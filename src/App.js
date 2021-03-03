346766aeb02a95dfb813e5d6ca5a78b65b5a9095
Lukasz Sadurski <lukaszsadurski@gmail.com>
2021-03-03 22:41:07 +0100

    added styled components to project, implemented new API

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import './App.css';
+import React, { useState, useEffect } from 'react';
 import { Button } from 'antd';
+import "antd/dist/antd.css";
+import './App.css';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Button>Default Button</Button>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [item, setItem] = useState([]);
+
+  // Note: the empty deps array [] means
+  // this useEffect will run once
+  // similar to componentDidMount()
+  useEffect(() => {
+    fetch("https://api.cryptonator.com/api/full/btc-pln")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          setIsLoaded(true);
+          console.log(result.ticker.price);
+          setItem(result.ticker);
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      )
+  }, [])
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  } else if (!isLoaded) {
+    return <div>Loading...</div>;
+  } else {
+    return (
+      <ul>
+        <li key={item.id}>
+
+          {item.price}
+          <Button>Click</Button>
+        </li>
+      </ul>
+    );
+  }
+
+  //   return (
+  //     <div className="App">
+  //       <header className="App-header">
+  //         <p>
+  //           Edit <code>src/App.js</code> and save to reload.
+  //         </p>
+  //         <Button>Default Button</Button>
+  //         <a
+  //           className="App-link"
+  //           href="https://reactjs.org"
+  //           target="_blank"
+  //           rel="noopener noreferrer"
+  //         >
+  //           Learn React
+  //         </a>
+  //       </header>
+  //     </div>
+  //   );
 }
 
 export default App;
